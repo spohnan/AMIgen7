490b750795c1f55a156ae157f93ec9b665ac98c6
Andy Spohn <andy.spohn@icf.com>
2017-12-29 13:04:37 -0500

    wip

--- buildAMI.sh
+++ buildAMI.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
 VOLUME=$1
+if [ -z $VOLUME ]; then
+    VOLUME=/dev/xvdb
+fi
 
 yum -y update
-
 yum -y install coreutils device-mapper device-mapper-event device-mapper-event-libs device-mapper-libs \
     device-mapper-persistent-data e2fsprogs gawk git grep grub grub2 grub2-tools grubby libudev lvm2 \
     lvm2-libs openssl parted sed sysvinit-tools unzip util-linux-ng yum-utils zip
 
-./DiskSetup.sh -b /boot -v VolGroup00 -d $VOLUME ; \
-./MkChrootTree.sh $VOLUME ; \
-./MkTabs.sh $VOLUME ; \
-./ChrootBuild.sh ; \
-./AWScliSetup.sh ; \
-./ChrootCfg.sh ; \
-./GrubSetup.sh $VOLUME ; \
-./NetSet.sh ; \
-./CleanChroot.sh ; \
-./PreRelabel.sh	 ; \
-./Umount.sh
+scripts/DiskSetup.sh -b /boot -v VolGroup00 -d $VOLUME
+scripts/MkChrootTree.sh $VOLUME
+scripts/MkTabs.sh $VOLUME
+scripts/ChrootBuild.sh
+scripts/AWScliSetup.sh
+scripts/ChrootCfg.sh
+scripts/GrubSetup.sh $VOLUME
+scripts/NetSet.sh
+scripts/CleanChroot.sh
+scripts/PreRelabel.sh
+scripts/Umount.sh
 
