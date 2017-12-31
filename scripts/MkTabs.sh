d7d2cb40f627d99fd68c604b8e7f1eec0fdf468a
Andy Spohn <andy.spohn@icf.com>
2017-12-31 10:12:33 -0500

    wip

--- scripts/MkTabs.sh
+++ scripts/MkTabs.sh
@@ -9,22 +9,19 @@ TARGSWAP=${2:-/dev/VolGroup00/swapVol}
 FSTAB="${CHROOT}/etc/fstab"
 
 # Check for arguments
-if [[ $# -lt 1 ]]
-then
-   echo "Missing parameter(s). Aborting..." > /dev/stderr
-   exit 1
+if [ $# -lt 1 ]; then
+    echo "Missing parameter(s). Aborting..." > /dev/stderr
+    exit 1
 fi
 
 # Make sure that chroot'ed /etc/directory exists
-if [[ ! -d ${CHROOT}/etc ]]
-then
-   mkdir -p "${CHROOT}/etc"
-   if [[ $? -ne 0 ]]
-   then
-      echo "Failed to create /etc" > /dev/stderr && exit 1
-   else
-      echo "Created ${CHROOT}/etc" 
-   fi
+if [ ! -d ${CHROOT}/etc ]; then
+    mkdir -p "${CHROOT}/etc"
+    if [ $? -ne 0 ]; then
+        echo "Failed to create /etc" > /dev/stderr && exit 1
+    else
+        echo "Created ${CHROOT}/etc"
+    fi
 fi
 
 # Create file-header
@@ -40,31 +37,16 @@ EOF
 _CHROOT=$(echo "${CHROOT}" | sed 's#^/##')
 
 # Read mtab matches into an array
-IFS=$'\n'; MTABLNS=( $(grep "${CHROOT}" /etc/mtab | grep ^/dev | \
-                   sed 's#'"${_CHROOT}"'##') )
+IFS=$'\n'; MTABLNS=( $(grep "${CHROOT}" /etc/mtab | grep ^/dev | sed 's#'"${_CHROOT}"'##') )
 
 for FSLINE in "${MTABLNS[@]}"
 do
-   BLKDEV=$(echo "${FSLINE}" | awk '{print $1}')
-   MNTPNT=$(echo "${FSLINE}" | awk '{print $2}' | sed 's#//#/#')
-   FSTYPE=$(echo "${FSLINE}" | awk '{print $3}')
-   printf "%s\t%s\t%s\tdefaults\t0 0\n" "${BLKDEV}" "${MNTPNT}" "${FSTYPE}"
+    BLKDEV=$(echo "${FSLINE}" | awk '{print $1}')
+    MNTPNT=$(echo "${FSLINE}" | awk '{print $2}' | sed 's#//#/#')
+    if [ $MNTPNT == '/boot' ]; then
+        BLKDEV=$(blkid $BLKDEV | awk '{print $3}' | cut -d '"' -f2)
+    fi
+    FSTYPE=$(echo "${FSLINE}" | awk '{print $3}')
+    printf "%s\t%s\t%s\tdefaults\t0 0\n" "${BLKDEV}" "${MNTPNT}" "${FSTYPE}"
 done >> "${FSTAB}"
 printf "%s\t%s\t%s\t%s\t0 0\n" "${TARGSWAP}" swap swap '-' >> "${FSTAB}"
-exit
-
-# Read mtab matches into an array
-IFS=$'\n'; MTABLNS=( $(grep "${CHROOT}" /etc/mtab | sed 's#'"${_CHROOT}"'##') )
-
-for FSLINE in "${MTABLNS[@]}"
-do
-   BLKDEV=$(echo "${FSLINE}" | awk '{print $1}')
-   MNTPNT=$(echo "${FSLINE}" | awk '{print $2}' | sed 's#//#/#')
-   FSTYPE=$(echo "${FSLINE}" | awk '{print $3}')
-   MNTOPT=$(echo "${FSLINE}" | awk '{print $4}')
-   FSFREQ=$(echo "${FSLINE}" | awk '{print $5}')
-   FSPASS=$(echo "${FSLINE}" | awk '{print $6}')
-
-   printf "%s %s %s %s %s %s\n" "${BLKDEV}" "${MNTPNT}" "${FSTYPE}" "${MNTOPT}" "${FSFREQ}" "${FSPASS}"
-done > "${CHROOT}/etc/mtab"
-
